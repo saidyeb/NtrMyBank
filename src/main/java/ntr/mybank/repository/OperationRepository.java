31017907d42123e33227564773beb29f0a46fcf9
Said YEBBAH <syebbah@ateja.fr>
2020-04-11 01:55:38 +0200

    feat (model) : add basic retrieve methods for operation repository

--- src/main/java/ntr/mybank/repository/OperationRepository.java
+++ src/main/java/ntr/mybank/repository/OperationRepository.java
@@ -1,8 +1,16 @@
 package ntr.mybank.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import ntr.mybank.model.Account;
 import ntr.mybank.model.Operation;
 
+@Repository
 public interface OperationRepository extends CrudRepository<Operation, Integer>{
+	List<Operation> findByAccount(Account account);
+	List<Operation> findByDateCreatedAccount(Date dateCreated, Account account);
 }
